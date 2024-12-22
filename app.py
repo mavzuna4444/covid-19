ff8601f0a5cf8b85290d38f2b3db19a1ca2c63cd
mavzuna4444 <162799817+mavzuna4444@users.noreply.github.com>
2024-12-22 20:07:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.ensemble import BaggingClassifier
 
 st.title('Prediction of COVID-19')
 
